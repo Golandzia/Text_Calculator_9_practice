cdf166713eb12f5360c5bd1deceebc48eb935644
Daniil Solovev <solovevdaniil01@gmail.com>
2022-06-04 16:15:58 +0300

    Добавьте файлы проекта.

---
+++ 9/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace _9
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputFirst = new System.Windows.Forms.TextBox();
+            this.InputSecond = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TotalOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter first word:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter second word:";
+            // 
+            // InputFirst
+            // 
+            this.InputFirst.Location = new System.Drawing.Point(179, 23);
+            this.InputFirst.Name = "InputFirst";
+            this.InputFirst.Size = new System.Drawing.Size(200, 29);
+            this.InputFirst.TabIndex = 2;
+            // 
+            // InputSecond
+            // 
+            this.InputSecond.Location = new System.Drawing.Point(179, 75);
+            this.InputSecond.Name = "InputSecond";
+            this.InputSecond.Size = new System.Drawing.Size(200, 29);
+            this.InputSecond.TabIndex = 3;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(16, 168);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(96, 34);
+            this.CalculateButton.TabIndex = 4;
+            this.CalculateButton.Text = "Calculate!";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(140, 168);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(96, 34);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.Text = "Clear all";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total:";
+            // 
+            // TotalOutput
+            // 
+            this.TotalOutput.Location = new System.Drawing.Point(179, 121);
+            this.TotalOutput.Name = "TotalOutput";
+            this.TotalOutput.Size = new System.Drawing.Size(200, 29);
+            this.TotalOutput.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(525, 214);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TotalOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.InputSecond);
+            this.Controls.Add(this.InputFirst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text calulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputFirst;
+        private System.Windows.Forms.TextBox InputSecond;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TotalOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 9/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _9
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        int calcbutclick = 0, clrbutclick = 0;
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clrbutclick++;
+                string logfile = "log.log".DayAdd();
+                StreamWriter log = new StreamWriter(logfile, true);
+                log.Write(DateTime.Now.ToString("yyyy.dd.MM; HH:mm:ss"));
+                log.WriteLine("\tClearButton was clicked");
+                log.Close();
+                //Очистка
+                InputFirst.Text = ""; InputSecond.Text = ""; TotalOutput.Text = "";
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Калькулятор
+                calcbutclick++;
+                string s1 = Convert.ToString(InputFirst.Text);
+                string s2 = Convert.ToString(InputSecond.Text);
+                if (InputFirst.Text == "")
+                    TotalOutput.Text = "Заполните 1 поле!";
+                else if (InputSecond.Text == "")
+                    TotalOutput.Text = "Заполните 2 поле!";
+                else
+                    TotalOutput.Text = s1 + s2;
+                string filename = "result.txt";
+                StreamWriter res = new StreamWriter(filename, true);
+                res.WriteLine(TotalOutput.Text);
+                res.Close();
+                string logfile = "log.log".DayAdd();
+                StreamWriter log = new StreamWriter(logfile, true);
+                log.Write(DateTime.Now.ToString("yyyy.dd.MM; HH:mm:ss"));
+                log.WriteLine("\tCalculateButton was clicked");
+                log.WriteLine($"Entered in TextBox1 word: {InputFirst.Text}; Entered in TextBox2 word: {InputSecond.Text};\nResulting word: {TotalOutput.Text}");
+                log.Close();
+            }
+            catch { }
+        }
+
+        
+    }
+    public static class timeForFiles
+    {
+        public static string TimeAdd(this string fileName)
+        {
+            return string.Concat(
+                Path.GetFileNameWithoutExtension(fileName),
+                DateTime.Now.ToString("-yyyy-dd-MM-HH-mm-ss"),
+                Path.GetExtension(fileName)
+                );
+        }
+        public static string DayAdd(this string fileName)
+        {
+            return string.Concat(
+                Path.GetFileNameWithoutExtension(fileName),
+                DateTime.Now.ToString("-yyyy-dd-MM"),
+                Path.GetExtension(fileName)
+                );
+        }
+    }
+}
